2a80fe9735c3880f3109ff9f8848611d5f8349b3
SCRATCHER 5-23 <86705803+5-23@users.noreply.github.com>
2022-01-11 15:11:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,9 +33,6 @@ yt_api_key_m = "AIzaSyCm9gKtQc9IJlvx5pCNc_X5SwPtADiMCMM"
 
 #==================================================================
 
-ran = 0
-back = 0
-
 scratcher = 577266050769485844
 noob = 711769839022243910
 junjacger = 829200004136173618
@@ -1300,7 +1297,7 @@ tan 각도
             else:
                 await message.channel.send(embed = Embed(title = "이미 다른곡이 재생중 입니다" , description = "곡을 멈추고 싶다면 ``5정지``를 사용하세요"))
             if not vc.is_playing():
-                voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+                voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS),after = lambda e:FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
 
     
     if message.content.startswith(f"{p}정지"):
